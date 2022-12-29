41d3f3099da5cfa386b0771c2f9940bb0f276a40
Felix Adam <felix.adam@tx.group>
2022-12-29 16:39:31 +0100

    feat: test-next-13 manual installation

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// import Head from 'next/head';
+// import { Inter } from '@next/font/google';
+// import styles from '../styles/Home.module.css';
+function HomePage() {
+  return <div>Welcome to Next.js, yes its basic... we know...</div>
+}
+
+export default HomePage
